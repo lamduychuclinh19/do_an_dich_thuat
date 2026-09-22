64d703250ef53529e5e6bc47a791b33bead0955b
unknown <chucv1995@gmail.com>
2026-09-22 18:23:21 +0700

    add 3 layers POIAPI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from app.routers.poi_router import router as poi_router
+
+
 app = FastAPI(
     title="Hệ thống thuyết minh đa ngôn ngữ",
     version="1.0.0"
 )
+
+app.include_router(poi_router)
 
 
 @app.get("/health")
